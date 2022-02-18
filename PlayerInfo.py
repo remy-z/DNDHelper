fa889dfd5934ef85ec6a2af8951a04dfbd42e33d
remy-z <remyszach@gmail.com>
2022-02-18 00:07:02 -0800

    storeLast

--- PlayerInfo.py
+++ PlayerInfo.py
@@ -2,3 +2,11 @@ class PlayerInfo:
     
     def __init__(self):
         self.playerInfo = {}
+
+    def storeLast(self, ctx):
+        guild, author, command, message = ctx.guild(), ctx.author(), ctx.command(), ctx.message()
+        if ctx.guild() not in self.playerInfo:
+            self.playerInfo[guild] = {}
+        if ctx.author() not in self.playerInfo[guild]:
+            self.playerInfo[guild][author] = {}
+        self.playerInfo[guild][author]["last"] = (command, message)
